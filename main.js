35453bfbcb4a3bdd9541a2b17c306b4ecaa55377
suna-kawa <sunagawa69852@gmail.com>
2022-12-23 15:55:58 +0900

    add finish calculator

--- main.js
+++ main.js
@@ -1,11 +1,28 @@
-//get value
-let valueNum = $("[name|='num']").val();
+function clickBtn(numBtn) {
+    //btn値を代入
+    let btnValue = numBtn.innerHTML;
+    //display部のidを代入
+    let result = document.getElementById('value');
+    //display部の一番右側にくる値を代入
+    let last_value = result.innerHTML.slice(-1);
 
 
-//calculator-display
-$(document).ready(function() {
-    $('.calculator-btn').on('click', function() {
-    $('.calculator-display td').append(valueNum);
-    console.log(valueNum);
-});
-});
+    if(btnValue == 'AC') {
+        result.innerHTML = '0'
+        return;
+    }
+    if(btnValue == '=') {
+        result.innerHTML = eval(result.innerHTML)
+        return;
+    }
+    if(result.innerHTML == '0') {
+        result.innerHTML = btnValue
+        return;
+    }
+    if((last_value == '/' || last_value == '+' || last_value == '-' || last_value == '*')&&
+      (btnValue == '/' || btnValue == '+' || btnValue == '-' || btnValue == '*')) {
+        result.innerHTML = result.innerHTML.slice(0, -1) + btnValue;
+        return;
+    }
+    result.innerHTML += btnValue;
+};
